e91a753f94936e22a6ad205dd77d969c5d4f4403
kornelu123 <kornelu12@gmail.com>
2026-03-01 22:31:30 +0100

    Blink LED instead of spewing gibberish on serial

--- pico-client/waterer/uart_task.c
+++ pico-client/waterer/uart_task.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <time.h>
 
+#include "pico/cyw43_arch.h"
+
 #include "sched.h"
 
 int
 uart_task(void)
 {
-  printf("Time since boot: %us\n", to_ms_since_boot(get_absolute_time()));
+  bool gpio_val = !cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN);
+  // Instead of spewing jibberish on serial console, just blink LED
+  cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, gpio_val);
 
-  return 3000;
+  if (gpio_val) {
+    return 100;
+  }
+
+  return 2900;
 }
